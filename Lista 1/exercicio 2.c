648d8b23badadf4faedebfa2159b6505618d160a
Luc1an0s <lucianomaciel53@gmail.com>
2025-08-26 20:17:29 -0400

    Create exercicio 2.c

---
+++ Lista 1/exercicio 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (void){
+	float x, y, sub = 0;
+	
+	//entrada de dados
+	printf("digite o primeiro numero que deseja subtrair\n");
+	scanf("%f", &x);
+	printf("digite o primeiro numero que deseja subtrair\n");
+	scanf("%f", &y);
+	
+	//processamento de saida
+	sub=x-y;
+	
+	printf("a subtracao e: %f", sub);
+	
+	return 0;
+}
