959f756f54914c7a422a84d0f361019e3dd751da
Luc1an0s <lucianomaciel53@gmail.com>
2025-08-26 20:12:31 -0400

    update in repositoty

---
+++ Lista 1/exercicio 1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void){
+	int x, y, soma = ;
+	
+	//entrada de dados
+	printf("Digite dois numeros inteiros, respectivamente (digite o numero e aperte enter)");
+	scanf("%d %d", &x, &y);
+	
+	//processamento de saída
+	soma=x+y;
+	
+	printf("a soma e: %d", soma);
+	
+	returno 0;
+}
