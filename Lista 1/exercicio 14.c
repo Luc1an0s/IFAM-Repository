f197a7130c4cda56df753f42040a044cefb61d0d
Luc1an0s <lucianomaciel53@gmail.com>
2025-08-27 22:26:27 -0400

    Create exercicio 14.c

---
+++ Lista 1/exercicio 14.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (void){
+	float peso, pesoGramas, gramasMais;
+	
+	//entrada de dados
+	printf("digite o seu peso\n");
+	scanf("%f", &peso);
+	
+	//processamento de dados
+	pesoGramas=peso*1000;
+	
+	gramaMais=pesoGramas*0.05;
+	
+	//saida de dados
+	printf("o seu peso em gramas e %.2f", pesoGramas);
+	printf("seu peso com 5 por cento e %.2f", gramasMais);
+	
+	return 0;
+}
