c8bbdc47b074e06d45f1fa6e6616be38635ad8e1
Luc1an0s <lucianomaciel53@gmail.com>
2025-08-31 19:24:29 -0400

    update atv5

--- lista estrutura de seleção/Atv5.c
+++ lista estrutura de seleção/Atv5.c
@@ -4,17 +4,17 @@ int main(void){
 	float salarioFuncionario, reajuste;
 	
 	//entrada de dados
-	printf("coloque o salario do funcionario");
+	printf("coloque o salario do funcionario\n");
 	scanf("%f", &salarioFuncionario);
 
 	//processamento e saida de dados
 	if(salarioFuncionario<=300){
 		reajuste=salarioFuncionario*1.5;
-		printf("o salario do funcionario com reajuste de 50 por cento e %f", reajuste);
+		printf("o salario do funcionario com reajuste de 50 por cento e %f\n", reajuste);
 	}
 	else{
 		reajuste=salarioFuncionario*1.3;
-		printf("o salario do funcionario com reajuste de 30 por cento e %f", reajuste);
+		printf("o salario do funcionario com reajuste de 30 por cento e %f\n", reajuste);
 	}
 	return 0;
 }
