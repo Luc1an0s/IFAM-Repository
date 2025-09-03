7ba55eb4b9f8e289ab0c238eed3f39c6b6aa1fe6
Luc1an0s <lucianomaciel53@gmail.com>
2025-09-03 13:31:03 -0400

    Create Atv8.c

---
+++ lista estrutura de seleção/Atv8.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+	float notaLab, notaAv, notaEx, mediaFinal;
+	
+	//entrada de dados
+	printf("Digite a nota do trabalho de laboratorio (0 a 10):\n");
+	scanf("%f", &notaLab);
+	
+	printf("Digite a nota da avaliacao semestral (0 a 10):\n");
+	scanf("%f", &notaAv);
+
+	printf("Digite a nota do exame final (0 a 10):\n");
+	scanf("%f", &notaEx);
+	
+	//processamento e saida de dados
+	mediaFinal=((notaLab*2)+(notaAv*3)+(notaEx*5))/10;
+	
+	printf("Media final e %.2f\n", mediaFinal);
+	
+	if(mediaFinal >= 8 && mediaFianl <== 10)
+		printf("Conceito A\n");
+	else if (mediaFinal >= 7)
+		printf("Conceito B\n");
+	else if (mediaFinal >= 6)
+		printf("Conceito C\n");
+	else if (mediaFinal >= 5)
+		printf("Conceito D\n");
+	else
+		printf("Conceito E\n");
+		
+	return 0;
+}
